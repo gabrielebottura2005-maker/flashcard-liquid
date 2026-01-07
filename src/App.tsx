a0f88c10eade068460c81dc8836d263679f139a7
gabrielebottura2005-maker <gabrielebottura2005@gmail.com>
2026-01-07 16:05:17 +0100

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import LiquidGlassCard from './LiquidGlassCard'
+
+export default function App() {
+  return (
+    <div className="app">
+      <h1>Flashcards</h1>
+      <LiquidGlassCard>
+        <p>Tap per iniziare 🚀</p>
+      </LiquidGlassCard>
+    </div>
+  )
+}
